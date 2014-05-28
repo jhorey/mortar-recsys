4fe9e72b3f4426f1fad5d1129d31b43b781d4be0
James Horey <jlh@opencore.io>
2014-05-28 10:41:22 +0000

    command to copy recommendation

--- ferry/testmortar.sh
+++ ferry/testmortar.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# Bash colors
+GREEN='\e[0;32m'
+NC='\e[0m'
+
 function run_as_ferry {
     echo -e "${GREEN} ${2} ${NC}"
     if [ $USER == "root" ]; then
@@ -9,17 +13,30 @@ function run_as_ferry {
     fi
 }
 
+# Make the output directory
+MKDIR='mkdir -p /service/data/out'
+run_as_ferry "$MKDIR" "Creating output directories"
+
+# Navigate to the Mortar directory.
+cd /home/ferry/mortar-recsys
+
 if [ $1 == "data" ]; then
-    MKDIR='hadoop dfs -mkdir -p /service/data/retail'
-    COPY1='hadoop dfs -copyFromLocal /home/ferry/mortar-recsys/data/retail/purchases.json /service/data/retail/'
-    COPY2='hadoop dfs -copyFromLocal /home/ferry/mortar-recsys/data/retail/wishlists.json /service/data/retail/'
-    COPY3='hadoop dfs -copyFromLocal /home/ferry/mortar-recsys/data/retail/inventory.json /service/data/retail/'
+    MKDIR='hdfs dfs -mkdir -p /service/data/retail'
+    COPY1='hdfs dfs -copyFromLocal /home/ferry/mortar-recsys/data/retail/purchases.json /service/data/retail/'
+    COPY2='hdfs dfs -copyFromLocal /home/ferry/mortar-recsys/data/retail/wishlists.json /service/data/retail/'
+    COPY3='hdfs dfs -copyFromLocal /home/ferry/mortar-recsys/data/retail/inventory.json /service/data/retail/'
 
     run_as_ferry "$MKDIR" "Making data directory"
     run_as_ferry "$COPY1" "Copy purchases dataset"
     run_as_ferry "$COPY2" "Copy wishlists dataset"
     run_as_ferry "$COPY3" "Copy inventory dataset"
 elif [ $1 == "retail" ]; then
-    PIG='pig -f pigscripts/retail-recsys.pig -m params/retail.params'
+    PIG='$PIG_HOME/bin/pig -f pigscripts/retail-recsys.pig -m params/retail.params'
     run_as_ferry "$PIG" "Running retail script"
+elif [ $1 == "users" ]; then
+    COPY='hdfs dfs -copyToLocal /service/data/retail/out/user_item_recs /service/data/out/'
+    run_as_ferry "$COPY" "Copying user recommendations"
+elif [ $1 == "items" ]; then
+    COPY='hdfs dfs -copyToLocal /service/data/retail/out/item_item_recs /service/data/out/'
+    run_as_ferry "$COPY" "Copying item recommendations"
 fi
